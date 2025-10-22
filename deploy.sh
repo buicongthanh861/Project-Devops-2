8745e7b96118d51f57f4ae48568aaa3a834d7ec9
buicongthanh861 <buicongthanh861@gmail.com>
2025-10-22 13:00:13 +0700

    releasing 2.1.3

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-BASE_DIR=$(pwd)
-
-# Xóa các resource cũ (nếu có)
-kubectl delete -f "$BASE_DIR/namespace.yaml" --ignore-not-found
-kubectl delete -f "$BASE_DIR/secret.yaml" --ignore-not-found
-kubectl delete -f "$BASE_DIR/service.yaml" --ignore-not-found
-
-# Apply lại các resource mới
-kubectl apply -f "$BASE_DIR/namespace.yaml"
-kubectl apply -f "$BASE_DIR/secret.yaml"
-kubectl apply -f "$BASE_DIR/service.yaml"
+kubectl apply -f namespace.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml

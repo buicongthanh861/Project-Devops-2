3164e3cb49ca70f1833d180535212345a7cdeae0
buicongthanh861 <buicongthanh861@gmail.com>
2025-10-22 12:19:49 +0700

    releasing 2.1.3

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-BASE_DIR=/home/ubuntu/kubernets
-
-kubectl apply -f $BASE_DIR/namespace.yaml
-kubectl apply -f $BASE_DIR/secret.yaml
-kubectl apply -f $BASE_DIR/deployment.yaml
-kubectl apply -f $BASE_DIR/service.yaml
+kubectl apply -f namespace.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml

bfe862cccf5c096fd414754a152623cc52de8dcc
buicongthanh861 <buicongthanh861@gmail.com>
2025-10-22 12:30:31 +0700

    releasing 2.1.3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-kubectl apply -f namespace.yaml
-kubectl apply -f secret.yaml
-kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
+BASE_DIR=~/kubernets
+cd $BASE_DIR
+
+[ -f "namespace.yaml" ] && kubectl apply -f namespace.yaml
+[ -f "secret.yaml" ] && kubectl apply -f secret.yaml
+[ -f "deployment.yaml" ] && kubectl apply -f deployment.yaml
+[ -f "service.yaml" ] && kubectl apply -f service.yaml
